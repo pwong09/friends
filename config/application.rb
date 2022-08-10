34281384ea009bf1700c743ab91fcd6cfedb93a6
pwong09 <pwong09@gmail.com>
2022-08-10 14:04:30 -0700

    Fix sign up redirect, unauth redirect

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module Friends
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.paths['app/views'] << "app/views/devise"
   end
 end
